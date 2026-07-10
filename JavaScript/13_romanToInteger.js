c66ff8b39e1f9ea2b1a59c06322236bdf513a496
Zihad-1883 <zihad.pilot18@gmail.com>
2026-07-10 23:40:14 +0600

    Roman to Integer Problem Solved

---
+++ JavaScript/13_romanToInteger.js
@@ -0,0 +1,54 @@
+var romanToInt = function (s) {
+  let output = 0;
+  const numericValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let current = numericValues[s[i]];
+    let next = numericValues[s[i + 1]];
+
+    if (current >= next || next === undefined) {
+      output += current;
+    } else {
+      output -= current;
+    }
+  }
+
+  return output;
+};
+
+console.log(romanToInt("MCMXCIV"));
+
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+
+//   const numericValues = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000,
+//     IV: 4,
+//     IX: 9,
+//     XL: 40,
+//     XC: 90,
+//     CD: 400,
+//     CM: 900,
+//   };
